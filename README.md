c4833b8944091df270659b97c0352bc04e485925
Peter McHale <peter.thomas.mchale@gmail.com>
2023-08-14 14:27:58 +0000

    do 5-hour run

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## Fine-tune an open-source 7B-parameter LLM for instruction following 
+## Parameter-efficient fine-tuning an open-source 7B-parameter LLM for instruction following 
 
-I trained a [7B LLM](https://huggingface.co/Salesforce/xgen-7b-8k-base) on a public [instruction dataset](https://huggingface.co/datasets/tatsu-lab/alpaca) using LoRA for 0.5 epochs over XXX hours on a single-GPU `g4dn.xlarge` EC2 instance. 
+I fine-tuned a [7B LLM](https://huggingface.co/Salesforce/xgen-7b-8k-base) on a public [instruction dataset](https://huggingface.co/datasets/tatsu-lab/alpaca) using LoRA for 0.5 epochs over 5 hours on a single-GPU `g4dn.xlarge` EC2 instance. 
 
 Due to the size of the LLM, I had to increase the volume size of the EC2 instance: 
 ```
@@ -10,7 +10,7 @@ Due to the size of the LLM, I had to increase the volume size of the EC2 instanc
 
 The training code uses the torch, transformers, peft and trl libraries. See `requirements.txt`.
 
-The training run is tracked [here](XXX) and the fine-tuned model is available [here](XXX).
+The training run is tracked [here](https://wandb.ai/peter-thomas-mchale/huggingface/runs/8gdlnzsd?workspace=user-peter-thomas-mchale) and the fine-tuned model adaptors are available [here](https://huggingface.co/petermchale/xgen-7b-tuned-alpaca).
 
 ## Credit
 
--- train.py
+++ train.py
@@ -5,26 +5,30 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
 from trl import SFTTrainer
 
 def train(): 
-  train_dataset = load_dataset("tatsu-lab/alpaca", split="train")
-  tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
-  tokenizer.pad_token = tokenizer.eos_token
-  model = AutoModelForCausalLM.from_pretrained(
+  print('Setting up model...')
+  original_model = AutoModelForCausalLM.from_pretrained(
     "Salesforce/xgen-7b-8k-base", 
     load_in_4bit=True, 
     torch_dtype=torch.float16,
     device_map="auto"
   )
-  model.resize_token_embeddings(len(tokenizer))
-  model = prepare_model_for_int8_training(model)
-  peft_config = LoraConfig(
+  tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
+  tokenizer.pad_token = tokenizer.eos_token
+  original_model.resize_token_embeddings(len(tokenizer))
+  original_model = prepare_model_for_int8_training(original_model)
+
+  print('Applying LoRA ...')
+  lora_config = LoraConfig(
     r=16, 
     lora_alpha=32, 
     lora_dropout=0.05, 
     bias="none",
     task_type="CAUSAL_LM",
   ) 
-  model = get_peft_model(model, peft_config)
+  peft_model = get_peft_model(original_model, lora_config)
 
+  print("Parameter-efficient fine-tuning (training)...")
+  train_dataset = load_dataset("tatsu-lab/alpaca", split="train")
   training_args = TrainingArguments(
     output_dir="xgen-7b-tuned-alpaca",
     per_device_train_batch_size=4, 
@@ -34,23 +38,26 @@ def train():
     fp16=True, 
     warmup_ratio=0.1,
     lr_scheduler_type="linear", 
-    num_train_epochs=0.5, 
+    num_train_epochs=0.15, 
     save_strategy="epoch",
     push_to_hub=True,
   )
-
   trainer = SFTTrainer(
-    model=model,
+    model=peft_model,
     train_dataset=train_dataset,
     dataset_text_field="text",
     max_seq_length=1024, 
     tokenizer=tokenizer,
     args=training_args,
     packing=True,
-    peft_config=peft_config,
-  )
+    peft_config=lora_config,
+  )  
   trainer.train()
+
+  print("Pushing LoRA adapators to Hub...")
   trainer.push_to_hub()
+
+  print("Done!")
 
 if __name__ == "__main__":
   train()
